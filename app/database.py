59e022011a38891a993519acfd06e6522a7f5c46
samraj <61903698+samrajkodai@users.noreply.github.com>
2022-08-25 20:25:32 +0530

    heroku database added

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ Server='MSI\SQLEXPRESS'
 Database='Fastapi'
 Driver1='SQL Server'
 
-Databaseconn=f'mssql://@{Server}/{Database}?driver={Driver1}'
+Databaseconn='postgres://aifhxkcpkmjxls:6861e98a65043b1ebe2f0d58e945195f98736fa4dd3d25d1490e5c7722e753d9@ec2-34-227-135-211.compute-1.amazonaws.com:5432/d723pbmneak647'
 
 engine=create_engine(Databaseconn)
 
